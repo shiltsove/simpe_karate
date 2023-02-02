5566140cf4d58cf91c61772df1edc42b3242f741
shiltsove <shiltsove@gmail.com>
2023-02-02 16:54:13 +1300

    new test batch

---
+++ src/test/java/features/NamedRunner.java
@@ -0,0 +1,24 @@
+package features;
+import com.intuit.karate.junit5.Karate;
+public class NamedRunner {
+	
+	@Karate.Test
+	Karate sampleTest() {
+//		path(s) to features file here. no need to put .feature ending
+		return Karate.run("sample").relativeTo(getClass());
+	}
+//	@Karate.Test
+//	Karate getAPITest() {
+////		path(s) to features file here. no need to put .feature ending
+//		return Karate.run("getapi").relativeTo(getClass());
+//	}
+//	
+//	@Karate.Test
+//	Karate deleteAPITest() {
+//		return Karate.run("createAndDeleteUser").relativeTo(getClass());
+//	}
+
+	
+	
+	
+}
